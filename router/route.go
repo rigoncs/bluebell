3b2bef1c217922a0554f528d6c31ad44b8dfbf9e
Rigon <2412789408@qq.com>
2024-12-18 15:05:20 +0800

    add database files

---
+++ router/route.go
@@ -0,0 +1,16 @@
+package router
+
+import (
+	"bluebell/logger"
+
+	"github.com/gin-gonic/gin"
+)
+
+func SetupRouter(mode string) *gin.Engine {
+	if mode == gin.ReleaseMode {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	r := gin.New()
+	r.Use(logger.GinLogger(), logger.GinRecovery(true))
+	return r
+}
