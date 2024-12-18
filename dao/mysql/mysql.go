910affc039d4c48d5d4b24c18a9d0a717dca84f0
Rigon <2412789408@qq.com>
2024-12-18 11:52:52 +0800

    init config files

---
+++ dao/mysql/mysql.go
@@ -0,0 +1,31 @@
+package mysql
+
+import (
+	"bluebell/setting"
+	"fmt"
+
+	"github.com/jmoiron/sqlx"
+)
+
+var db *sqlx.DB
+
+func Init(cfg *setting.MySQLConfig) (err error) {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True",
+		cfg.User,
+		cfg.Password,
+		cfg.Host,
+		cfg.Port,
+		cfg.DB,
+	)
+	db, err = sqlx.Connect("mysql", dsn)
+	if err != nil {
+		return
+	}
+	db.SetMaxOpenConns(cfg.MaxOpenConns)
+	db.SetMaxIdleConns(cfg.MaxIdleConns)
+	return
+}
+
+func Close() {
+	_ = db.Close()
+}
