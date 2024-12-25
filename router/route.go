01414f825bed008c0fc6a31b147526b4880d03a4
Rigon <2412789408@qq.com>
2024-12-25 21:09:31 +0800

    实现创建帖子功能

--- router/route.go
+++ router/route.go
@@ -25,10 +25,10 @@ func SetupRouter(mode string) *gin.Engine {
 	v1.POST("/login", controller.LoginHandler)
 	v1.GET("/community", controller.CommunityHandler)
 	v1.GET("/community/:id", controller.CommunityDetailHandler)
-	v1.GET("/post/:id", controller.GetPostDetailHandler)
-	v1.POST("/post", controller.CreatePostHandler)
 
 	v1.Use(middlewares.JWTAuthMiddleware())
+	v1.GET("/post/:id", controller.GetPostDetailHandler)
+	v1.POST("/post", controller.CreatePostHandler)
 
 	r.NoRoute(func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{
